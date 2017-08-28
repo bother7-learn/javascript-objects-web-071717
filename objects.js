06f4d61cb401c3669aba8c629cd9eb6d76e67fbf
bother7 <github email address>
2017-08-28 13:01:21 -0400

    Done

---
+++ objects.js
@@ -0,0 +1,11 @@
+let playlist = {"kesha":"tik tok"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  playlist
+}
